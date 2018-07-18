eddfce6df800838b7297dc7cea51ebb39bd14b71
Adrian Sims <adriansims62781@gmail.com>
2018-07-18 11:00:11 +0100

    Add project files.

---
+++ GetLUL/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetLUL
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var filename = "s:\\RJFAF903.LOC";
+                foreach (var line in File.ReadAllLines(filename))
+                {
+                    if (line.StartsWith("RL70"))
+                    {
+                        var region = line[85];
+                        var crs = line.Substring(56, 3);
+                        var zone = line[83];
+                        var zoneNlc = line.Substring(262, 6);
+                        var endDate = line.Substring(9, 8);
+
+                        if (endDate == "31122999" && zoneNlc.All(x=>x != 'Y') && "123456".Contains(zone) && (region == '6' || crs[0] == 'Z'))
+                        {
+                            Console.WriteLine(line);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add GetLUL/Program.cs
+git add GetLUL/App.config
+git add GetLUL/Properties/AssemblyInfo.cs
+git add GetLUL/GetLUL.csproj
+git add GetLUL.sln
+git add .gitignore
